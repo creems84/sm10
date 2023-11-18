3674b3963f85fa6246c4d5d3eec85965a3aefdbb
creems84 <128843897+creems84@users.noreply.github.com>
2023-11-19 00:02:48 +0300

    Create seminar10.py

---
+++ seminar10.py
@@ -0,0 +1,14 @@
+import random
+import pandas as pd
+
+lst = ['robot'] * 10
+lst += ['human'] * 10
+random.shuffle(lst)
+data = pd.DataFrame({'whoAmI': lst})
+unique_values = data['whoAmI'].unique()
+
+for value in unique_values:
+    data[value] = (data['whoAmI'] == value).astype(int)
+
+data = data.drop('whoAmI', axis=1)
+data.head()
